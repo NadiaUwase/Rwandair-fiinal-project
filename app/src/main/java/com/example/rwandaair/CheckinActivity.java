81aef31c9c3ece991234b77a0790d0f681c26311
peterwilliam860 <peterwilliam860@gmail.com>
2020-03-09 03:40:19 +0200

    boarding pass layout added

--- app/src/main/java/com/example/rwandaair/CheckinActivity.java
+++ app/src/main/java/com/example/rwandaair/CheckinActivity.java
@@ -5,9 +5,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -16,13 +18,16 @@ import com.google.android.material.textfield.TextInputLayout;
 public class CheckinActivity extends AppCompatActivity {
 
     TextInputLayout referenceNoTextInputField;
+    Button checkInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkin);
         referenceNoTextInputField = (TextInputLayout) findViewById(R.id.referenceNoTextInput);
+        checkInButton = (Button) findViewById(R.id.startCheckInBtn);
         checkInMoreInfoButtonClick();
+        startCheckInClick();
     }
 
     public void checkInMoreInfoButtonClick() {
@@ -31,6 +36,17 @@ public class CheckinActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast toast = Toast.makeText(getApplicationContext(), "Show check in guide", Toast.LENGTH_SHORT);
                 toast.show();
+            }
+        });
+    }
+
+    public void startCheckInClick() {
+        checkInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CheckinActivity.this,
+                        BoardingPassActivity.class);
+                startActivity(intent); // startActivity allow you to move
             }
         });
     }
