fae5c83b661e84868275f9451cb0be4e5c6bac7b
Jinet Onyango <43377714+Jonyango@users.noreply.github.com>
2020-03-10 12:51:23 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/rwandaair/LuggageQuestionActivity.java
@@ -0,0 +1,15 @@
+package com.example.rwandaair;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LuggageQuestionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luggage_question);
+
+    }
+}
